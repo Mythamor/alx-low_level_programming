bae2428f446cbaaddd4efea05f669a1f7dc7e130
Mythamor <bethmithamo.bm@gmail.com>
2023-02-19 13:21:28 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Description: Print alphabet in lower case except for q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void) 
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++) 
+
+	if (c == 'e' || c == 'q') {
+	continue;
+	}
+
+	putchar(c);
+	}
+
+	putchar('\n');
+
+	return 0;
+}
