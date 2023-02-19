2a19a4e2009c156fd1c5c0bfaa120a37bfce8e23
Mythamor <bethmithamo.bm@gmail.com>
2023-02-19 14:50:11 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+ * main- Entry point
+ *
+ * Declaration: print all the numbers of base 16 in lowercase
+ *
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+	{
+			putchar(i + '0');/*prints 0-9*/
+	}
+			else
+	{
+				putchar(i - 10 + 'a');/*prints a-f*/
+	}
+	}
+	putchar('\n');
+	return (0);
+}
