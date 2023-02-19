df2f9f42a3262af1287fd721a6aef8bacc41de14
Mythamor <bethmithamo.bm@gmail.com>
2023-02-19 14:08:50 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Declaration:  print 0123456789 using putchar
+
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
