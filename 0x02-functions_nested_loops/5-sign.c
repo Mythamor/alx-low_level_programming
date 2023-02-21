27ed9db026c4a83c68cdd49c915ff5a3138ead3c
Mythamor <bethmithamo.bm@gmail.com>
2023-02-21 14:18:09 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,8 +4,8 @@
  * print_sign - prints the sign of a number
  * @n: number to check
  * Return: 1 if n greater than  0.
- * 		0 if n equal to  0
- * 		-1 n less than 0
+ *		0 if n equal to  0
+ *		-1 n less than 0
  */
 
 int print_sign(int n)
