a4c58a00134eb54e5a0b9c5d21e4ec55ff038e6d
Mythamor <bethmithamo.bm@gmail.com>
2023-02-23 14:35:12 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,15 +12,12 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-
 /*
-*Description: While loop commented out.
 *	while (i < 10)
 *{
 *	putchar(i);
 *}
 */
-
 printf("\nInfinite loop avoided!  \\0/\n");
 
 return (0);
