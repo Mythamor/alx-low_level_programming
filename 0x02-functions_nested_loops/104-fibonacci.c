c9c673938f9b717872387345f11317c141bd2e49
Mythamor <bethmithamo.bm@gmail.com>
2023-03-23 08:30:08 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	double num1 = 1, num2 = 2, nextNum, count;
+
+	printf("%.0lf, %.0lf, ", num1, num2);
+
+	for (count = 3; count <= 98; count++)
+{
+	nextNum = num1 + num2;
+	printf("%.0lf", nextNum);
+
+	if (count < 98)
+{
+	printf(", ");
+}
+
+	num1 = num2;
+	num2 = nextNum;
+}
+	printf("\n");
+	return (0);
+}
