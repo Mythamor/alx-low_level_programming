bb1679c5a138928594114c89637eee281f9d8d8e
Mythamor <bethmithamo.bm@gmail.com>
2023-03-30 15:55:27 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,18 @@
 
 size_t print_list(const list_t *h)
 {
-	int i;
-	char *str1;
+ size_t s = 0;
 
-	for (i = 0; h; i++, h = h->next)
-	{
-		str1 = h->str;
-		if (str1)
-			printf("[%i] %s\n", h->len, str1);
-		else
-			printf("[0] %p\n", str1);
-	}
-	return (i);
+ while (h)
+ {
+ if (!h->str)
+ printf("[0] (nil)\n");
+ else
+ printf("[%u] %s\n", h->len, h->str);
+ h = h->next;
+ s++;
+ }
+
+ return (s);
+
 }
