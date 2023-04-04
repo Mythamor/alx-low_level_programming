c30c7ebfe27550a3f9df6c848ade7db530f91513
Mythamor <bethmithamo.bm@gmail.com>
2023-04-04 15:54:19 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,20 +4,19 @@
 #include <stddef.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  */
 
-typedef struct list_s
+typedef struct listint_s
 {
-        char *str;
-        unsigned int len;
-        struct list_s *next;
-} list_t;
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
@@ -35,5 +34,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
 
 #endif
