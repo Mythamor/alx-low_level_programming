905c101304a617633ec375363250cddd40613734
Mythamor <bethmithamo.bm@gmail.com>
2023-04-10 18:16:10 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,39 +13,41 @@
   * Return: number of letters it could read and print or 0 if fails
   */
 
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-{
+	char *buffer;
+	int fop;
+	size_t fread, check;
+
+	if (!filename || !letters)
 		return (0);
-}
 
-	int frd = open(filename, O_RDONLY);
-
-	if (frd == -1)
-{
+	buffer = malloc(letters);
+	if (!buffer)
 		return (0);
-}
-
-	char buffer[letters];
-
-	ssize_t bytes_read = read(frd, buffer, letters);
-
-	if (bytes_read == -1)
-{
-		close(frd);
+	/* open file */
+	fop = open(filename, O_RDONLY);
+	if (fop == -1)
+	{
+		free(buffer);
 		return (0);
-}
-
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-	if (bytes_written == -1 || bytes_written != bytes_read)
-{
-		close(frd);
+	}
+	/* read file up to "letters" amount of charachters */
+	fread = read(fop, buffer, letters);
+	if (fread < 1)
+	{
+		free(buffer);
+		close(fop);
 		return (0);
-}
+	}
+	/* write content read */
+	check = write(STDOUT_FILENO, buffer, fread);
 
-	close(frd);
-	return (bytes_written);
+	free(buffer);
+	close(fop);
+
+	if (!check || check != fread)
+		return (0);
+
+	return (fread);
 }
