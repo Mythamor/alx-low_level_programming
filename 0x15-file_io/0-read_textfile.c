f45287c5f5433d197a7cc2c21e0d3e496e7d3022
Mythamor <bethmithamo.bm@gmail.com>
2023-04-10 18:09:45 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,32 +16,36 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-if (filename == NULL)
+	if (filename == NULL)
 {
-return (0);
+		return (0);
 }
 
-int frd = open(filename, O_RDONLY);
+	int frd = open(filename, O_RDONLY);
 
-if (frd == -1)
+	if (frd == -1)
 {
-return (0);
-}
-char buffer[letters];
-
-ssize_t bytes_read = read(frd, buffer, letters);
-if (bytes_read == -1)
-{
-close(frd);
-return (0);
+		return (0);
 }
 
-ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
+	char buffer[letters];
+
+	ssize_t bytes_read = read(frd, buffer, letters);
+
+	if (bytes_read == -1)
 {
-close(frd);
-return (0);
+		close(frd);
+		return (0);
 }
-close(frd);
-return (bytes_written);
+
+	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	if (bytes_written == -1 || bytes_written != bytes_read)
+{
+		close(frd);
+		return (0);
+}
+
+	close(frd);
+	return (bytes_written);
 }
