a14cf6945410a8cf1b69191bc111cfbf96b5d94e
Mythamor <bethmithamo.bm@gmail.com>
2023-04-10 18:55:07 -0700

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,38 +7,29 @@
   *
   * Return: 1 on succes, -1 on failue
   */
-
 int create_file(const char *filename, char *text_content)
 {
 	int file, len = 0, check = 0;
 
 	if (!filename)
-{
 		return (-1);
-}
 
-/* creates file or truncates a file, and give it permission -rw------ */
-
-file = open(filename, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
-
+	/* creates file or truncates a file, and give it permission -rw------ */
+	file = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (file == -1)
-{
 		return (-1);
-}
 
-/* if text, writes text into file */
+	/* if text, writes text into file */
 	if (text_content)
-{
+	{
 		for (len = 0; text_content[len]; len++)
+			;
 		check = write(file, text_content, len);
-		if (check == -1)
-{
-			close(file);
-			return (-1);
-}
-}
-/* closes file */
+	}
+	/* closes file */
 	close(file);
+	if (check != len)
+		return (-1);
 
 	return (1);
 }
