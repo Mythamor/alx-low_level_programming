3625038062ed481dcc892727b0ba574412e30d0f
Mythamor <bethmithamo.bm@gmail.com>
2023-04-10 18:43:01 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+  * create_file - creates a file or truncates it
+  * @filename: the name of the file to create
+  * @text_content: NULL terminated string to write to file
+  *
+  * Return: 1 on succes, -1 on failue
+  */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, len = 0, check = 0;
+
+	if (!filename)
+{
+		return (-1);
+}
+
+/* creates file or truncates a file, and give it permission -rw------ */
+
+file = open(filename, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+
+	if (file == -1)
+{
+		return (-1);
+}
+
+/* if text, writes text into file */
+	if (text_content)
+{
+		for (len = 0; text_content[len]; len++)
+		check = write(file, text_content, len);
+		if (check == -1)
+{
+			close(file);
+			return (-1);
+}
+}
+/* closes file */
+	close(file);
+
+	return (1);
+}
