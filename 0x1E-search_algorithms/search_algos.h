ee441270381204ae413c10f82ee32321e073d902
Mythamor <bethmithamo.bm@gmail.com>
2023-09-13 19:23:18 +0300

    Search algorithim eader file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,18 +5,35 @@
 #include <stdlib.h>
 #include <math.h>
 
+/**
+ * struct listint_s - singly linked list node
+ * @n: Integer stored in the node
+ * @next: pointer to the next node in the list
+ *
+ * Description: singly linked list node structure
+ */
+
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
+
+/**
+ * struct skiplist_s - singly linked list with an express lane
+ * @n: Integer stored in the node
+ * @express: a pointer to next nnode in express lane
+ * @next: ipointer to the next node in the list
+ *
+ *  Description: singly linked list node structure
+ */
 
 typedef struct skiplist_s
 {
 	int n;
 	struct skiplist_s *express;
 	struct skiplist_s *next;
-}skiplist_t;
+} skiplist_t;
 
 
 int linear_search(int *array, size_t size, int value);
